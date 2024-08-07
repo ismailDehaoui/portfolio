ca2a1f044336638639988b5822dbd63a8570f4fe
Ismail Dehaoui <81970637+ismailDehaoui@users.noreply.github.com>
2024-08-07 20:16:20 +0100

    Hero Designe

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
 import './hero.css'
 const Hero = () => {
   return (
-    <div>hero</div>
+    <section className='hero flex'>
+      <div className='left-section'>
+        <div className="parent-avetar flex">
+          <img src="./ismail.jpg" alt="" className="avetar" />
+          <div className='icon-verified'></div>
+        </div>
+        <h1 className='title'>
+          Software Engineer
+        </h1>
+        <p className='sub-title'>
+          I'm Ismail Dehaoui, a software engineer with a strong customer focus and expertise in backend development. Skilled in testing and maintenance, with a sharp analytical mindset. Committed to keeping up with industry trends and delivering innovative solutions.
+        </p>
+        <div className='all-icons flex'>
+          <div className="icon icon-x"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-facebook"></div>
+          <div className="icon icon-linkedin2"></div>
+        </div>
+      </div>
+      <div className='right-section animation border'>animation</div>
+    </section>
   )
 }
 
